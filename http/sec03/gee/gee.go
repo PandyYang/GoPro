79efd7c4aeff7fbae9a670e15bdd3c404ebb8d5f
PandyYang <yangpandy@gmail.com>
2022-12-09 14:15:39 +0800

    前缀树路由demo

---
+++ http/sec03/gee/gee.go
@@ -0,0 +1,46 @@
+package gee
+
+import "net/http"
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router *router
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
